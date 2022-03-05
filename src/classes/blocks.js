d23d5a575b702e484a3cd79c68a337f1eb6345c5
Neogen121 <mkasatka@gmail.com>
2022-03-05 17:26:06 +0300

    create

---
+++ src/classes/blocks.js
@@ -0,0 +1,55 @@
+import { row, col, css } from "../utils";
+
+export class Block {
+    constructor(value, options) {
+        this.value = value;
+        this.options = options;
+    }
+
+    toHTML() {
+        throw new Error("метод toHTML не реализован");
+    }
+}
+
+export class TitleBlock extends Block {
+    constructor(value, options) {
+        super(value, options);
+    }
+
+    toHTML() {
+        const { tag = "h1", styles } = this.options;
+        return row(col(`<${tag}>${this.value}</${tag}>`), css(styles));
+    }
+}
+
+export class ImageBlock extends Block {
+    constructor(value, options) {
+        super(value, options);
+    }
+
+    toHTML() {
+        const { imageStyles: is, alt = "", styles } = this.options;
+        return row(`<img src="${this.value}" alt="${alt}" style="${css(is)}"></img>`, css(styles));
+    }
+}
+
+export class ColumnsBlock extends Block {
+    constructor(value, options) {
+        super(value, options);
+    }
+
+    toHTML() {
+        let html = this.value.map(col).join("");
+        return row(html, css(this.options.styles));
+    }
+}
+
+export class TextBlock extends Block {
+    constructor(value, options) {
+        super(value, options);
+    }
+
+    toHTML() {
+        return row(col(`<p>${this.value}</p>`), css(this.options.styles));
+    }
+}
